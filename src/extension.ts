13a34a2286fa9993bb647607d3ae5530d40027e9
gintama91 <b9-20@srkrec.edu.in>
2023-07-13 20:09:48 +0530

    add run command

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "Scarpe" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "Scarpe" is now active!');
+  let disposable = vscode.commands.registerCommand('Scarpee.runCurrent', () => {
+    const activeTextEditor = vscode.window.activeTextEditor;
+    if (activeTextEditor) {
+      const filePath = activeTextEditor.document.uri.fsPath;
+      const fileName = path.basename(filePath);
+      const workspaceFolders = vscode.workspace.workspaceFolders;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('Scarpe.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Scarpe_Vscode_Extension!');
-	});
+      if (workspaceFolders && workspaceFolders.length > 0) {
+        const folderPath = workspaceFolders[0].uri.fsPath;
 
-	context.subscriptions.push(disposable);
+        const command = `./exe/scarpe ${filePath} --dev`;
+
+        const terminal = vscode.window.createTerminal({
+          name: 'Scarpe Terminal',
+          cwd: folderPath,
+        });
+
+        terminal.sendText(command);
+        terminal.show();
+
+        vscode.window.showInformationMessage(`Running ${fileName} with Scarpe.`);
+      } else {
+        vscode.window.showWarningMessage('No workspace is currently open.');
+      }
+    } else {
+      vscode.window.showWarningMessage('No file is currently open.');
+    }
+  });
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
