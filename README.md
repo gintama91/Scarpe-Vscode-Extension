1c4a55cabf94581985b8523aaece2e6e2ee42f82
gintama91 <b9-20@srkrec.edu.in>
2023-07-14 07:49:23 +0530

    add debug and normal

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Scarpe VSCode Extension
 
-This is a simple vscode extension for [scarpe](https:://github.com/scarpe-team/scarpe) to run shoes code in vs code for development.
+This is a simple vscode extension for scarpe, to run shoes code in vs code for development.
 
 Open a Shoes file and open the command palette and run `Scarpe run current file` to run the code in development mode.
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,66 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 
+function runScarpeCommand(filePath: string, command: string) {
+  const fileName = path.basename(filePath);
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+
+  if (workspaceFolders && workspaceFolders.length > 0) {
+    const folderPath = workspaceFolders[0].uri.fsPath;
+
+    const terminal = vscode.window.createTerminal({
+      name: 'Scarpe Terminal',
+      cwd: folderPath,
+    });
+
+    terminal.sendText(command);
+    terminal.show();
+
+    vscode.window.showInformationMessage(`Running ${fileName} with Scarpe.`);
+  } else {
+    vscode.window.showWarningMessage('No workspace is currently open.');
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "Scarpe" is now active!');
 
-  let disposable = vscode.commands.registerCommand('Scarpee.runCurrent', () => {
+  let runCurrentDisposable = vscode.commands.registerCommand('Scarpee.runCurrent', () => {
     const activeTextEditor = vscode.window.activeTextEditor;
     if (activeTextEditor) {
       const filePath = activeTextEditor.document.uri.fsPath;
-      const fileName = path.basename(filePath);
-      const workspaceFolders = vscode.workspace.workspaceFolders;
-
-      if (workspaceFolders && workspaceFolders.length > 0) {
-        const folderPath = workspaceFolders[0].uri.fsPath;
-
-        const command = `./exe/scarpe ${filePath} --dev`;
-
-        const terminal = vscode.window.createTerminal({
-          name: 'Scarpe Terminal',
-          cwd: folderPath,
-        });
-
-        terminal.sendText(command);
-        terminal.show();
-
-        vscode.window.showInformationMessage(`Running ${fileName} with Scarpe.`);
-      } else {
-        vscode.window.showWarningMessage('No workspace is currently open.');
-      }
+      const command = `./exe/scarpe ${filePath} --dev`;
+      runScarpeCommand(filePath, command);
     } else {
       vscode.window.showWarningMessage('No file is currently open.');
     }
   });
 
-  context.subscriptions.push(disposable);
+  let debugDisposable = vscode.commands.registerCommand('Scarpee.debug', () => {
+    const activeTextEditor = vscode.window.activeTextEditor;
+    if (activeTextEditor) {
+      const filePath = activeTextEditor.document.uri.fsPath;
+      const command = `./exe/scarpe ${filePath} --dev --debug`;
+      runScarpeCommand(filePath, command);
+    } else {
+      vscode.window.showWarningMessage('No file is currently open.');
+    }
+  });
+
+  let productionDisposable = vscode.commands.registerCommand('Scarpee.production', () => {
+    const activeTextEditor = vscode.window.activeTextEditor;
+    if (activeTextEditor) {
+      const filePath = activeTextEditor.document.uri.fsPath;
+      const command = `scarpe ${filePath}`;
+      runScarpeCommand(filePath, command);
+    } else {
+      vscode.window.showWarningMessage('No file is currently open.');
+    }
+  });
+
+  context.subscriptions.push(runCurrentDisposable);
+  context.subscriptions.push(debugDisposable);
+  context.subscriptions.push(productionDisposable);
 }
 
 export function deactivate() {}
